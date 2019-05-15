0847f14974229c37d8bb0c020b49306e509b6aa2
Rahul Nayak <pragyes31@users.noreply.github.com>
2019-05-15 18:53:58 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -6,11 +6,14 @@ import "./styles.css";
 function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Hello Chatter Box</h1>
+      <h3>Let's chitter chatter</h3>
     </div>
   );
 }
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
+
+// Complete Skype app by Friday 17th May
+// Reach halfway of Book 2 by Ohans by Saturday EOD
